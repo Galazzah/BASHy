20015895c12dc7faf271e66682174e9b6f1b0084
ics45c <mzaccari@uci.edu>
2015-11-22 02:26:51 -0800

    updated

--- bingTraffic.sh
+++ bingTraffic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bingTraffic(){
-curl http://dev.virtualearth.net/REST/v1/Traffic/Incidents/mapArea/includeLocationCodes?severity=severity1,severity2,severityn&type=type1,type2,typen&key=eqBURiWWTE5afcJ2IqYG~xNlJhANCoR7y-NsuhTEswQ~AtEBP9BiaYkAEDKsgHuJWI0lxmMWjE83qzFw3yUbZdoowdXKaCnaNbJKtUo5eb24 >> $(pwd)/result.json
+curl http://dev.virtualearth.net/REST/v1/Traffic/Incidents/mapArea/includeLocationCodes?severity=severity1,severity2,severityn\&type=type1,type2,typen\&key=eqBURiWWTE5afcJ2IqYG~xNlJhANCoR7y-NsuhTEswQ~AtEBP9BiaYkAEDKsgHuJWI0lxmMWjE83qzFw3yUbZdoowdXKaCnaNbJKtUo5eb24 >> $(pwd)/result.json
 
 }
