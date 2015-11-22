9fb1bd1004aa7cb8bc143506f2748e24366bb6df
Galazzah <ghiyath28@gmail.com>
2015-11-22 03:06:46 -0800

    night night

--- bingTraffic/bingTraffic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-bingTraffic(){
-curl http://dev.virtualearth.net/REST/v1/Traffic/Incidents/37,-105,45,-94?key=eqBURiWWTE5afcJ2IqYG~xNlJhANCoR7y-NsuhTEswQ~AtEBP9BiaYkAEDKsgHuJWI0lxmMWjE83qzFw3yUbZdoowdXKaCnaNbJKtUo5eb24 >> $(pwd)/result.json
-
-}
